4f623e1b5573349fbde9413c8f2976e218292ff2
jenichegomes-sketch <jenichegomes@gmail.com>
2026-03-31 13:11:23 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const text = ["Web Developer", "Frontend Developer", "Student"];
+let i = 0;
+let j = 0;
+let current = "";
+let isDeleting = false;
+
+function type() {
+  current = text[i];
+
+  if (!isDeleting) {
+    document.querySelector(".typing").textContent = current.substring(0, j++);
+    if (j > current.length) {
+      isDeleting = true;
+      setTimeout(type, 1000);
+      return;
+    }
+  } else {
+    document.querySelector(".typing").textContent = current.substring(0, j--);
+    if (j == 0) {
+      isDeleting = false;
+      i = (i + 1) % text.length;
+    }
+  }
+
+  setTimeout(type, isDeleting ? 50 : 100);
+}
+
+type();
